c58459e0b6764667d611049f48dadeaf4836ccb9
Lalksy <ler2161@columbia.edu>
2017-11-09 00:40:10 -0500

    Adding more routes

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -2,30 +2,69 @@ import React, {Component} from 'react'
 import { Switch, Route } from 'react-router-dom'
 // import LoginView from './LoginView'
 import Main_Page from './Main_Page'
-import MainViewer from './MainViewer';
+import MainViewer from './MainViewer'
 import ProfileView from './ProfileView'
 import HeaderView from './HeaderView'
+import LoginView from './LoginView'
+import DashboardView from './DashboardView'
 
 class Main extends Component {
     constructor(props){
 	super(props);
 	this.state = {
 	    loggedIn: false,
+	    showLoginModal: false,
 	};
-     }
+	//console.log("Constructor");
+
+	this.loginHandler = this.loginHandler.bind(this);
+	this.logoutHandler = this.logoutHandler.bind(this);
+	this.loginToggleHandler = this.loginToggleHandler.bind(this);
+	this.promptLogin = this.promptLogin.bind(this);
+    }
+
+    loginHandler(e) {
+	e.preventDefault();
+	this.setState({
+		loggedIn: true,
+		    })
+	    }
+
+
+    logoutHandler(e) {
+	e.preventDefault();
+	this.setState({
+		loggedIn: false,
+		    })
+	    }
+
+    promptLogin(e) {
+	console.log("Hi!");
+	e.preventDefault();
+	this.setState({
+		showLoginModal: true,
+		    })
+	    }
+
+    loginToggleHandler(e) {
+	console.log("Hi!");
+	e.preventDefault();
+	this.setState((prevState) => {
+		return {loggedIn: !prevState.loggedIn};
+	    });
+    }
 
   render () {
     return (
       <div className="Main">
-      <HeaderView />
+      <HeaderView loggedIn={this.state.loggedIn} promptLoginHandler = {this.promptLogin}/>
       <Switch>
       <Route exact path='/'
         render=
           {
             (props) => (
-              <MainViewer
-                 loggedIn={this.state.loggedIn}
-	      />
+	      <LoginView loginHandler = {this.loginHandler} show={this.state.showLoginModal}/>
+              
             )
           }
       />
@@ -39,6 +78,15 @@ class Main extends Component {
 	     )
 	  }
       />
+      <Route exact path='/home'
+        render=
+	  {
+	  (props) => (
+       	     <DashboardView />
+	         )
+          }
+      />
+
       <Route exact path='/Main_Page' component={Main_Page}/>
 
     </Switch>
