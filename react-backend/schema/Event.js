64370a53e2bcd544d3cc55d736022fee58930f88
ZhangChi <ChyZin@dyn-129-236-235-37.dyn.columbia.edu>
2017-11-12 22:15:09 -0500

    event backend

--- react-backend/schema/Event.js
+++ react-backend/schema/Event.js
@@ -5,7 +5,70 @@ var EventSchema = new Schema({
   id: String,
   host: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   name: String,
-  teams: [{type: mongoose.Schema.Types.ObjectId, ref: 'Team'}]
+  teams: [{type: mongoose.Schema.Types.ObjectId, ref: 'Team'}],
+  registration_deadline:{ type: Date },
+  start_date: { type: Date },
+  end_date: { type: Date },
+  description: String,
+  max: Number,
+  min: Number
 })
 
 var Model = mongoose.model("Event", EventSchema);
+module.exports = {
+  add: function(fields,callback){
+    var one = new Model(fields);
+    one.save(function(error,user) {
+      if (error) {
+        console.log("Got error 3: " + error + "\n")
+        if (callback) callback(error,null)
+      } else {
+        if (callback) callback(null,user)
+      }
+    });
+    // one.save(callback);
+  },
+  update: function(id, fields, callback) {
+    Model.findOne({
+      _id: id
+    }).exec(function(error, one) {
+      if (error) {
+        callback(error);
+      } else {
+        one.update(fields, callback);
+      }
+    });
+  },
+  find: function(criteria,callback){
+    Model.find(criteria).exec(function (error, some) {
+      callback(error, some);
+    });
+  },
+  findById: function(id, callback) {
+    Model.findOne({ _id : id }).
+    exec(function (error, one) {
+      callback(error, one);
+    });
+  },
+  middleware: {
+
+		loadAll: function(req, res, next){
+      Model.find({}).populate([{
+        path:'host',
+        model:'User',
+        populate: [{
+          path:'profile',
+          model:'Profile'
+        },{
+          path:'credential',
+          model:'Credential'
+        }]
+      }]).exec(function (error, all) {
+      req.events = all || [];
+      next();
+    });
+  }
+
+
+  }
+}
