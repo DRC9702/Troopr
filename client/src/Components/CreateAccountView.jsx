e1cc89b22fcce614bdf4073baf5e300aca432e8c
Lalksy <ler2161@columbia.edu>
2017-11-12 21:56:02 -0500

    fixing linting errors

---
+++ client/src/Components/CreateAccountView.jsx
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import { ControlLabel, FormControl, Button, HelpBlock, Jumbotron } from 'react-bootstrap';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const styles = {
+  backgroundColor: 'orange',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  flexGrow: 1,
+};
+
+class CreateAccountView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      username: '',
+      password: '',
+    };
+  }
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+  handleUserChange(e) {
+    this.setState({ username: e.target.value });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  createAccount(e) {
+    // var self
+    const data = {
+      email: this.state.email,
+      username: this.state.username,
+      password: this.state.password,
+    };
+    e.preventDefault();
+    // self = this
+
+    console.log(this.state);
+
+    // Submit form via jQuery/AJAX
+    console.log(data);
+    axios.post('/api/create_account', {
+      email: this.state.email,
+      username: this.state.username,
+      password: this.state.password,
+    })
+      .then((response) => {
+        console.log(response);
+        console.log(response.data.success);
+        console.log(response.data.user);
+
+        if (response.data.success) {
+          window.location = '/create_profile';
+        } else {
+          alert('Account already exists');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="CreateAccountView" style={styles}>
+        <Jumbotron style={{ width: '100vh' }}>
+          <h1>Create Account</h1>
+          {/* <p><Button bsStyle="primary">Learn more</Button></p> */}
+          <form>
+            <ControlLabel>Email Address</ControlLabel>
+            <FormControl
+              type="text"
+              value={this.state.email}
+              placeholder="troop@troopr.edu"
+              onChange={this.handleEmailChange}
+            />
+            <FormControl.Feedback />
+            <HelpBlock>Please enter a valid email address.</HelpBlock>
+            <ControlLabel>Username</ControlLabel>
+            <FormControl
+              type="text"
+              value={this.state.username}
+              placeholder="Username"
+              onChange={this.handleUserChange}
+            />
+            <FormControl.Feedback />
+            <HelpBlock>Please enter a username.</HelpBlock>
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              type="password"
+              value={this.state.password}
+              placeholder="Password"
+              onChange={this.handlePasswordChange}
+            />
+            <FormControl.Feedback />
+
+            <HelpBlock>Please confirm your password.</HelpBlock>
+            <Button onClick={this.createAccount}>
+              <Link to="/home">
+                    Create Account
+              </Link>
+            </Button>
+          </form>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+// function FieldGroup({ id, label, help, ...props }) {
+//     return (
+//         <FormGroup controlId={id}>
+//             <ControlLabel>{label}</ControlLabel>
+//             <FormControl {...props} />
+//             {help && <HelpBlock>{help}</HelpBlock>}
+//         </FormGroup>
+//     );
+// }
+
+export default CreateAccountView;
