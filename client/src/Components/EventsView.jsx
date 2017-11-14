844fef0041ca6504b75301d05a8ad16b4ccf76a4
Lalksy <ler2161@columbia.edu>
2017-11-14 13:39:07 -0500

    in the middle of eventsview

--- client/src/Components/EventsView.jsx
+++ client/src/Components/EventsView.jsx
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import {Table} from 'react-bootstrap';
+import axios from 'axios';
 
 class EventsView extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    const value = new Date().toISOString();
+    this.state = {
+        events: []
+    };
+    this.getEvents = this.getEvents.bind(this);
+  }
+
+    getEvents = function(e) {
+      // Submit form via jQuery/AJAX
+              //console.log(data);
+              axios.post('/api/search_event', {
+                  query: ""
+              })
+              .then((response) => {
+                  console.log(response);
+                  console.log(response.data.success);
+                  // console.log(response.data.user);
+              })
+              .catch((error) => {
+                  console.log(error);
+              });
+          }
 
     render() {
         return (
-            <div className="EventsView" style={styles}>
+            <div className="EventsView" style={styles} onLoad={this.getEvents}>
               <Table striped bordered condensed hover>
                 <thead>
                   <tr>
