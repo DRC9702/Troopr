ff22632fcce031fafd2c84af431b6350c04c6fef
DRC9702 <drinconcruz@knox.edu>
2017-11-28 21:22:32 -0800

    Fixing the beginning of weird issues with Victor's profileview

--- client/src/Components/ProfileView.jsx
+++ client/src/Components/ProfileView.jsx
@@ -5,46 +5,43 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class ProfileView extends Component {
-
-  constructor(props){
-      super(props);
-      this.state = {
-          name: '',
-          skills:'',
-          resume:'',
-          bio:''
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      skills: '',
+      resume: '',
+      bio: '',
+    };
   }
   componentDidMount() {
-    var _this = this;
     axios.post('/api/profile', {
     })
-      .then(function (response) {
-        if(response.data.success){
-          _this.setState({name:response.data.name});
-          _this.setState({skills:response.data.skills});
-          _this.setState({resume:response.data.resume});
-          _this.setState({bio:response.data.bio});
-          console.log(response.data.name)
-          console.log(response.data.skills)
-          console.log(response.data.resume)
-          console.log(response.data.bio)
-
-        }else{
-          console.log("failed2")
-
+      .then((response) => {
+        if (response.data.success) {
+          this.setState({
+            name: response.data.name,
+            skills: response.data.skills,
+            resume: response.data.resume,
+            bio: response.data.bio,
+          });
+          console.log(response.data.name);
+          console.log(response.data.skills);
+          console.log(response.data.resume);
+          console.log(response.data.bio);
+        } else {
+          console.log('failed2');
         }
-
       })
       .catch(function (error) {
         console.log(error)
         console.log("failed1")
-      })
+      });
   }
 
   render() {
     return (
-      <div className="ProfileView" style={styles}>
+      <div className="ProfileView">
         <p>Name: {this.state.name}</p>
         <p>SkillsList: {this.state.skills}</p>
         <p>Bio: {this.state.bio}</p>
@@ -53,25 +50,5 @@ class ProfileView extends Component {
     );
   }
 }
-
-const styles = {
-    backgroundColor: 'orange',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexWrap: 'wrap',
-    flexGrow: 1,
-};
-
-// function FieldGroup({ id, label, help, ...props }) {
-//     return (
-//         <FormGroup controlId={id}>
-//             <ControlLabel>{label}</ControlLabel>
-//             <FormControl {...props} />
-//             {help && <HelpBlock>{help}</HelpBlock>}
-//         </FormGroup>
-//     );
-// }
 
 export default ProfileView;
