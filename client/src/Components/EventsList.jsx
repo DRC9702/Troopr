601ab218317b40e1b20e9ed79279556502ca1487
DRC9702 <drinconcruz@knox.edu>
2017-11-30 01:45:30 -0800

    Cleaning some eslint complaints

--- client/src/Components/EventsList.jsx
+++ client/src/Components/EventsList.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Button, Modal } from 'react-bootstrap';
-import {withRouter} from "react-router-dom";
+import { withRouter } from 'react-router-dom';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+
 require('../styles/EventList.css');
 
 class EventsList extends Component {
@@ -15,6 +17,7 @@ class EventsList extends Component {
 
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
+    this.joinEvent = this.joinEvent.bind(this);
   }
 
   showModal(e, index) {
@@ -30,11 +33,13 @@ class EventsList extends Component {
     });
   }
 
-  joinEvent = (e) => {
-    console.log("abc")
+  joinEvent(e) {
+    console.log('abc');
 
-    if(e) e.preventDefault();
-    console.log(e.target.value)
+    if (e) {
+      e.preventDefault();
+    }
+    console.log(e.target.value);
 
     axios.post('/api/join_event', {
       event_id: e.target.value,
@@ -44,7 +49,7 @@ class EventsList extends Component {
         console.log(response.data.success);
 
         if (response.data.success) {
-          this.props.history.push("/dashboard");
+          this.props.history.push('/dashboard');
         } else {
           alert(response.data.message);
         }
@@ -56,45 +61,44 @@ class EventsList extends Component {
 
   render() {
     const listItems = this.props.events.map((event, index) => (
-
-        <tr key={event._id}>
-          <th>
-            <Button type="button" bsStyle="link" onClick={e => this.showModal(e, index)}>
-              {event.name}
+      <tr key={event._id}>
+        <th>
+          <Button type="button" bsStyle="link" onClick={e => this.showModal(e, index)}>
+            {event.name}
+          </Button>
+        </th>
+        <th>{event.start_date}</th>
+        <th>{event.end_date}</th>
+        <th>{event.registration_deadline}</th>
+        <Modal show={this.state.display === index} onHide={this.hideModal}>
+          <Modal.Header>
+            <Modal.Title>Event Detail</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <label>Host</label>
+            {JSON.stringify(this.props.events) ? (<p>{event.host.profile.name}</p>) : (<p />)}
+            <label>Start Date</label>
+            {JSON.stringify(this.props.events) ? (<p>{event.start_date}</p>) : (<p />)}
+            <label>End Date</label>
+            {JSON.stringify(this.props.events) ? (<p>{event.end_date}</p>) : (<p />)}
+            <label>Deadline for Team Registration</label>
+            {JSON.stringify(this.props.events) ? (<p>{event.registration_deadline}</p>) : (<p />)}
+            <label>Teams</label>
+            {JSON.stringify(this.props.events) ? (<p>{JSON.stringify(event.teams)}</p>) : (<p />)}
+            <label>Event Description</label>
+            {JSON.stringify(this.props.events) ? (<p>{event.description}</p>) : (<p />)}
+            <label>Max Number of Team Members</label>
+            {JSON.stringify(this.props.events) ? (<p>{event.max}</p>) : (<p />)}
+            <label>Min Number of Team Members</label>
+            {JSON.stringify(this.props.events) ? (<p>{event.min}</p>) : (<p />)}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="success" type="submit" value={event._id} onClick={this.joinEvent}>
+              Join
             </Button>
-          </th>
-          <th>{event.start_date}</th>
-          <th>{event.end_date}</th>
-          <th>{event.registration_deadline}</th>
-          <Modal show={this.state.display === index} onHide={this.hideModal}>
-            <Modal.Header>
-              <Modal.Title>Event Detail</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <label>Host</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.host.profile.name}</p>) : (<p></p>)}
-              <label>Start Date</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.start_date}</p>) : (<p></p>)}
-              <label>End Date</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.end_date}</p>) : (<p></p>)}
-              <label>Deadline for Team Registration</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.registration_deadline}</p>) : (<p></p>)}
-              <label>Teams</label>
-              {JSON.stringify(this.props.events) ? (<p>{JSON.stringify(event.teams)}</p>) : (<p></p>)}
-              <label>Event Description</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.description}</p>) : (<p></p>)}
-              <label>Max Number of Team Members</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.max}</p>) : (<p></p>)}
-              <label>Min Number of Team Members</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.min}</p>) : (<p></p>)}
-            </Modal.Body>
-            <Modal.Footer>
-              <Button bsStyle="success" type="submit" value={event._id} onClick={this.joinEvent}>
-                Join
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </tr>
+          </Modal.Footer>
+        </Modal>
+      </tr>
     ));
 
     return (
@@ -104,6 +108,7 @@ class EventsList extends Component {
 }
 
 EventsList.propTypes = {
+  history: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
   // showModal: PropTypes.func.isRequired,
 };
 
