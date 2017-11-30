4e382ed54439421c778f776908a7819c9a6cd4c3
DRC9702 <drinconcruz@knox.edu>
2017-11-29 23:44:38 -0800

    Fixing CreateEventView style

--- client/src/Components/CreateEventView.jsx
+++ client/src/Components/CreateEventView.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import {ControlLabel, FormControl, Button, FormGroup, HelpBlock, Col, Glyphicon} from 'react-bootstrap';
+import { Panel, ControlLabel, FormControl, Button, FormGroup, HelpBlock, Row, Col, Glyphicon} from 'react-bootstrap';
 import DatePicker from 'react-bootstrap-date-picker';
 import axios from 'axios';
+
+require('../styles/CreateEventView.css');
 
 class CreateEventView extends Component {
 
@@ -101,99 +103,90 @@ class CreateEventView extends Component {
 
     render() {
         return (
-            <div className="CreateEventView" style={styles}>
-                <br/><br/><br/><br/>
-                    <FieldGroup
-                        id="formControlsText"
-                        label="Event Name"
-                        type="text"
-                        placeholder="Event Name"
-                        value={this.state.eventName}
-                        onChange={this.handleEventNameChange}
-                    />
+          <div className="CreateEventView">
+            <h1>Create Event</h1>
+              <Panel header="Info" bsStyle="success" style={{ width: '75%', margin: '20px' }}>
+                <FieldGroup
+                    id="formControlsText"
+                    label="Event Name"
+                    type="text"
+                    placeholder="Event Name"
+                    value={this.state.eventName}
+                    onChange={this.handleEventNameChange}
+                />
+                <Row className="show-grid" style={{ width: '100%' }}>
+                  <Col sm={4}>
+                      <FormGroup validationState="success">
+                          <ControlLabel>Starting Date</ControlLabel>
+                          <DatePicker id="startDate" value={this.state.startDate} onChange={this.handleStartChange}
+                                      Styles="Success" clearButtonElement={<Glyphicon glyph="star" />}/>
+                      </FormGroup>
+                  </Col>
 
-                    <Col sm={3}>
-                        <FormGroup validationState="success">
-                            <ControlLabel>Starting Date</ControlLabel>
-                            <DatePicker id="startDate" value={this.state.startDate} onChange={this.handleStartChange}
-                                        Styles="Success" clearButtonElement={<Glyphicon glyph="star" />}/>
-                        </FormGroup>
-                    </Col>
+                  <Col sm={4}>
+                      <FormGroup validationState="success">
+                          <ControlLabel>Closing Date</ControlLabel>
+                          <DatePicker id="endDate" value={this.state.endDate} onChange={this.handleEndChange}
+                                      Styles="Success" clearButtonElement={<Glyphicon glyph="star" />}/>
+                      </FormGroup>
+                  </Col>
 
-                    <Col sm={3}>
-                        <FormGroup validationState="success">
-                            <ControlLabel>Closing Date</ControlLabel>
-                            <DatePicker id="endDate" value={this.state.endDate} onChange={this.handleEndChange}
-                                        Styles="Success" clearButtonElement={<Glyphicon glyph="star" />}/>
-                        </FormGroup>
-                    </Col>
+                  <Col sm={4}>
+                      <FormGroup validationState="success">
+                          <ControlLabel>Registration Deadline</ControlLabel>
+                          <DatePicker id="deadline" value={this.state.deadline} onChange={this.handleDeadlineChange}
+                                      Styles="Success" clearButtonElement={<Glyphicon glyph="star" />}/>
+                      </FormGroup>
+                  </Col>
+                </Row>
 
-                    <Col sm={3}>
-                        <FormGroup validationState="success">
-                            <ControlLabel>Registration Deadline</ControlLabel>
-                            <DatePicker id="deadline" value={this.state.deadline} onChange={this.handleDeadlineChange}
-                                        Styles="Success" clearButtonElement={<Glyphicon glyph="star" />}/>
-                        </FormGroup>
-                    </Col>
+                <FormGroup controlId="formControlsSelect">
+                    <ControlLabel>Min Team Size</ControlLabel>
+                    <FormControl componentClass="select" placeholder="select" value={this.state.minSize}
+                                  onChange={this.handleMinSizeChange}>
+                        <option value="select">select</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                    </FormControl>
+                </FormGroup>
 
-                    <FormGroup controlId="formControlsSelect">
-                        <ControlLabel>Min Team Size</ControlLabel>
-                        <FormControl componentClass="select" placeholder="select" value={this.state.minSize}
-                                     onChange={this.handleMinSizeChange}>
-                            <option value="select">select</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
-                        </FormControl>
-                    </FormGroup>
+                <FormGroup controlId="formControlsSelect">
+                    <ControlLabel>Max Team Size</ControlLabel>
+                    <FormControl componentClass="select" placeholder="select" value={this.state.maxSize}
+                                  onChange={this.handleMaxSizeChange}>
+                        <option value="select">select</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                    </FormControl>
+                </FormGroup>
 
-                    <FormGroup controlId="formControlsSelect">
-                        <ControlLabel>Max Team Size</ControlLabel>
-                        <FormControl componentClass="select" placeholder="select" value={this.state.maxSize}
-                                     onChange={this.handleMaxSizeChange}>
-                            <option value="select">select</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
-                        </FormControl>
-                    </FormGroup>
-
-                    <FormGroup controlId="formControlsTextarea">
-                        <ControlLabel >Event Description</ControlLabel>
-                        <FormControl componentClass="textarea" placeholder="textarea" bsSize="large"
-                                     value={this.state.eventBio} onChange={this.handleEventBioChange}/>
-                    </FormGroup>
-
-                <Button bsStyle="success" type="submit" onClick={this.createEvent}>
-                    Create
-                </Button>
-            </div>
+                <FormGroup controlId="formControlsTextarea">
+                    <ControlLabel >Event Description</ControlLabel>
+                    <FormControl componentClass="textarea" placeholder="textarea" bsSize="large"
+                                  value={this.state.eventBio} onChange={this.handleEventBioChange}/>
+                </FormGroup>
+              </Panel>
+              <Button bsStyle="success" type="submit" onClick={this.createEvent}>
+                  Create
+              </Button>
+          </div>
         );
     }
 }
-
-const styles = {
-    backgroundColor: 'orange',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexWrap: 'wrap',
-    flexGrow: 1,
-};
-
 
 function FieldGroup({ id, label, help, ...props }) {
     return (
