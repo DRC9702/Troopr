d24bc3946df0a8c592d9e5050b6d696dcb822ff3
DRC9702 <drinconcruz@knox.edu>
2017-11-30 19:18:05 -0800

    Disabled some unfixable lint rules and cleaned up BoxView with Victor

--- client/src/Components/BoxView.jsx
+++ client/src/Components/BoxView.jsx
@@ -2,23 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Panel, ListGroup, ListGroupItem, Button, Modal } from 'react-bootstrap';
 
-// require('../styles/SideView.css');
-
 class BoxView extends Component {
+  static parseArray(inStr) {
+    let str = inStr.substring(1, inStr.length - 1);
+    let ids = str.split(',');
+    str = ids.join(', ');
+    ids = str.split('"');
+    str = ids.join(' ');
+    // str = str.split("\""),join("");
+    return str;
+  }
 
   constructor(props) {
     super(props);
     this.state = {
       display: null,
     };
-
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
   }
 
   showModal(e, index) {
-    console.log('ShowModal is clicked');
-    console.log(index);
+    // console.log('ShowModal is clicked');
+    // console.log(index);
     this.setState({ display: index });
     // this.setState({ selectedEvent: eventId });
   }
@@ -29,29 +35,22 @@ class BoxView extends Component {
     });
   }
 
-  parseArray(str) {
-    str = str.substring(1, str.length - 1);
-    var ids = str.split(",");
-    str = ids.join(", ");
-    ids = str.split("\"");
-    str = ids.join(" ");
-    //str = str.split("\""),join("");
-    return str;
-  }
-
   render() {
     let list;
 
-    if (this.props.title === "Teams") {
-      list = this.props.teams.map((team) => (
+    if (this.props.title === 'Teams') {
+      list = this.props.teams.map(team => (
         <ListGroup key={team._id}>
-          <ListGroupItem href={'/team/' + team.event._id}>{team.members.map((member) => (
-            <p key={member._id}>{member.profile.name}</p>
-          ))}</ListGroupItem>
+          <ListGroupItem href={`/team/${team.event._id}`}>
+            {team.members.map(member => (
+              <p key={member._id}>
+                {member.profile.name}
+              </p>
+            ))}
+          </ListGroupItem>
         </ListGroup>
       ));
-    }
-    else if (this.props.title === "Events Hosted") {
+    } else if (this.props.title === 'Events Hosted') {
       list = this.props.events.map((event, index) => (
         <ListGroup key={event._id}>
           <ListGroupItem>
@@ -64,23 +63,31 @@ class BoxView extends Component {
               <Modal.Title>Event Detail</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              {JSON.stringify(this.props.events) ? (<p>{event.start_date}</p>) : (<p />)}
-              <label>End Date</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.end_date}</p>) : (<p />)}
-              <label>Deadline for Team Registration</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.registration_deadline}</p>) : (<p />)}
-              <label>Teams</label>
-              {JSON.stringify(this.props.events) ? (<p>{this.parseArray(JSON.stringify(event.teams))}</p>) : (<p />)}
-              <label>Event Description</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.description}</p>) : (<p />)}
-              <label>Max Number of Team Members</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.max}</p>) : (<p />)}
-              <label>Min Number of Team Members</label>
-              {JSON.stringify(this.props.events) ? (<p>{event.min}</p>) : (<p />)}
+              <b>Start Date</b>
+              {this.props.events ?
+                (<p>{event.start_date}</p>) : (<p />)}
+              <b>End Date</b>
+              {this.props.events ?
+                (<p>{event.end_date}</p>) : (<p />)}
+              <b>Deadline for Team Registration</b>
+              {this.props.events ?
+                (<p>{event.registration_deadline}</p>) : (<p />)}
+              <b>Teams</b>
+              {this.props.events ?
+                (<p>{BoxView.parseArray(JSON.stringify(event.teams))}</p>) : (<p />)}
+              <b>Event Description</b>
+              {this.props.events ?
+                (<p>{event.description}</p>) : (<p />)}
+              <b>Max Number of Team Members</b>
+              {this.props.events ?
+                (<p>{event.max}</p>) : (<p />)}
+              <b>Min Number of Team Members</b>
+              {this.props.events ?
+                (<p>{event.min}</p>) : (<p />)}
             </Modal.Body>
           </Modal>
         </ListGroup>
-      ))
+      ));
     }
 
     return (
@@ -95,7 +102,8 @@ class BoxView extends Component {
 
 BoxView.propTypes = {
   title: PropTypes.string.isRequired,
-  children: PropTypes.element.isRequired,
+  teams: PropTypes.array.isRequired,
+  events: PropTypes.array.isRequired,
 };
 
 export default BoxView;
