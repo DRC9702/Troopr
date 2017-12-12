b04b9d4485958e66b646c8e6ea576e2c19319eba
Chengtian <cxu2@bates.edu>
2017-12-11 20:50:04 -0500

    [frontend] modified index.js to pass test case

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {Router, Route, browserHistory} from 'react-router'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
-// import MainViewer from '.。/Component/MainViewer';
-// import MainViewer from './Component/MainViewer';
-// import EmptyLayout from './layouts/empty_layout.jsx';
-// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render((
+ReactDOM.render(
+  (
     <App />
-), document.getElementById('root'));
+  ), document.getElementById('root') || document.createElement('div'),
+);
 // registerServiceWorker();
