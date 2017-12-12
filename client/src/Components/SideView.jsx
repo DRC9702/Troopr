10fed6d33a4dbb7dad3c0200850dbb8b12472d16
DRC9702 <drinconcruz@knox.edu>
2017-12-11 21:35:39 -0800

    Adding sideview features

--- client/src/Components/SideView.jsx
+++ client/src/Components/SideView.jsx
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Image } from 'react-bootstrap';
+import axios from 'axios';
+
 
 require('../styles/SideView.css');
 
-
 class SideView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+    };
+    this.getName = this.getName.bind(this);
+  }
+
+  componentWillMount() {
+    this.getName();
+  }
+
+  getName() {
+    axios.get('/get_name')
+      .then((response) => {
+        // console.log(response);
+        if (response.data.success) {
+          this.setState({ username: response.data.name });
+        } else {
+          console.log('Failed to get username'); // eslint-disable-line no-console
+        }
+      }).catch((error) => {
+        console.log('Failed to get username'); // eslint-disable-line no-console
+        console.log(error); // eslint-disable-line no-console
+      });
+  }
+
   render() {
     return (
-      <div className="SideView" style={{ backgroundColor: 'grey' }}>
+      <div className="SideView" style={{ backgroundColor: 'black', opacity: '0.7' }}>
         <div className="ImageAndName">
-          <p>
-            Username
-          </p>
-          <Image src="https://picsum.photos/200/200/?random" circle style={{ width: '200px' }} />
+          <h1 style={{ color: 'white' }}>
+            {this.state.username}
+          </h1>
+          <Image src="https://picsum.photos/200/200/?random" circle style={{ width: '200px', padding: '10px' }} />
         </div >
         <br />
         <br />
-        <Button bsStyle="info" onClick={this.props.handleViewProfile} block>Profile</Button>
-        <Button bsStyle="info" onClick={this.props.handleViewEvent} block>View Events</Button>
-        <Button bsStyle="info" onClick={this.props.handleCreateEvent} block>Create Event</Button>
+        <div style={{ padding: '10px' }} >
+          <Button bsStyle="info" onClick={this.props.handleViewProfile} block>Profile</Button>
+          <br />
+          <Button bsStyle="info" onClick={this.props.handleViewEvent} block>View Events</Button>
+          <br />
+          <Button bsStyle="info" onClick={this.props.handleCreateEvent} block>Create Event</Button>
+        </div>
 
       </div>
     );
