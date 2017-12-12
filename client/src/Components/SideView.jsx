84d9a746b211eb9f90b1a15a0d7561594bf4a75c
DRC9702 <drinconcruz@knox.edu>
2017-12-11 21:07:04 -0800

    Lint bug

--- client/src/Components/SideView.jsx
+++ client/src/Components/SideView.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Image } from 'react-bootstrap';
-import logo from '../Images/default.png';
 
 require('../styles/SideView.css');
 
